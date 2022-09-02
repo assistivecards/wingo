4b972dcd22e868a3eb3dd86aca1cfbf0e57bf7d4
Burak Tokak <iletisim@buraktokak.com>
2022-09-02 20:35:21 +0300

    show edit only when there ar items

--- layouts/Home.js
+++ layouts/Home.js
@@ -253,7 +253,7 @@ const Home = ({ navigation }) => {
       }}>
 
 
-        <TouchableScale
+        {displayData && displayData.length != 0 && <TouchableScale
           style={{
             position: "absolute",
             bottom: 40,
@@ -284,7 +284,7 @@ const Home = ({ navigation }) => {
           {isEditing && <Svg viewBox="0 0 24 24" width={32} height={32}>
             <Path fill={API.config.panelColor} d="M9 16.2l-3.5-3.5c-.39-.39-1.01-.39-1.4 0-.39.39-.39 1.01 0 1.4l4.19 4.19c.39.39 1.02.39 1.41 0L20.3 7.7c.39-.39.39-1.01 0-1.4-.39-.39-1.01-.39-1.4 0L9 16.2z"></Path>
           </Svg>}
-        </TouchableScale>
+        </TouchableScale>}
 
         <TouchableScale
           style={{
