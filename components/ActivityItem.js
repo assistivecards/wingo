9f4f5f85d92125e986ec0fd3672be31ad8787d6e
Burak Tokak <iletisim@buraktokak.com>
2022-11-12 14:10:16 +0300

    fix dragging item issue

--- components/ActivityItem.js
+++ components/ActivityItem.js
@@ -40,29 +40,29 @@ const ActivityItem = ({
   return (
     <View style={{ flexDirection: 'row' }}>
       {(showEditing && isEditing) && (
-        <TouchableOpacity
-          style={{
-            transform: [{
-              scale: isActive ? 1.15 : 1
-            }],
-            zIndex: 100
-          }}
-          activeOpacity={0.9}
-          onLongPress={() => { API.haptics("impact"); drag(); }}
-        >
           <View
             style={{
               position: 'absolute',
               left: 10,
               height: "100%",
               width: 200,
-              justifyContent: "center"
+              justifyContent: "center",
+              zIndex: 100
             }}>
-            <Svg height={32} width={32} viewBox="0 0 24 24" style={{ opacity: 0.9 }}>
-              <Path fill={API.config.backgroundColor} d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2zm-2-8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></Path>
-            </Svg>
-          </View>
-        </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  transform: [{
+                    scale: isActive ? 1.15 : 1
+                  }],
+                  height: "100%", justifyContent: "center"
+                }}
+                activeOpacity={0.9}
+                onLongPress={() => { API.haptics("impact"); drag(); }}>
+              <Svg height={32} width={32} viewBox="0 0 24 24" style={{ opacity: 0.9 }}>
+                <Path fill={API.config.backgroundColor} d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2zm-2-8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></Path>
+              </Svg>
+            </TouchableOpacity>
+        </View>
       )}
 
       <TouchableScale style={{ width: '100%' }} onPress={handlePress}>
@@ -101,19 +101,20 @@ const ActivityItem = ({
       </TouchableScale>
 
       {(showEditing && isEditing) && (
-        <TouchableOpacity style={{ zIndex: 100 }} onPress={() => onRemoveItem && onRemoveItem(data.slug)}>
           <View
             style={{
               position: 'absolute',
               right: 10,
               height: "100%",
-              justifyContent: "center"
+              justifyContent: "center",
+              zIndex: 100
             }}>
-            <Svg height={32} width={32} viewBox="0 0 24 24" style={{ opacity: 0.9 }}>
-              <Path fill={API.config.backgroundColor} d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4 11H8c-.55 0-1-.45-1-1s.45-1 1-1h8c.55 0 1 .45 1 1s-.45 1-1 1z"></Path>
-            </Svg>
+            <TouchableOpacity style={{height: "100%", justifyContent: "center"}} onPress={() => onRemoveItem && onRemoveItem(data.slug)}>
+              <Svg height={32} width={32} viewBox="0 0 24 24" style={{ opacity: 0.9 }}>
+                <Path fill={API.config.backgroundColor} d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4 11H8c-.55 0-1-.45-1-1s.45-1 1-1h8c.55 0 1 .45 1 1s-.45 1-1 1z"></Path>
+              </Svg>
+            </TouchableOpacity>
           </View>
-        </TouchableOpacity>
       )}
 
     </View>
